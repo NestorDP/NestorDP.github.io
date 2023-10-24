f8920eb3100275d4cc8b9f8262c39725f1935148
NestorDP <nestor-dp@hotmail.com>
2023-10-24 10:37:50 -0300

    Chosen basic timeline

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+var events = document.querySelectorAll('.event');
+
+events.forEach(function(event) {
+ var year = event.getAttribute('data-year');
+ var timelineYear = document.createElement('div');
+ var timelineBar = document.createElement('div');
+ 
+ timelineYear.textContent = year;
+ timelineYear.className = 'timeline-year';
+ 
+ timelineBar.className = 'timeline-bar';
+ 
+ document.querySelector('.timeline').appendChild(timelineYear);
+ document.querySelector('.timeline').appendChild(timelineBar);
+});
